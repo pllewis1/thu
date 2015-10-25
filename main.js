34e5694e6a9a969de1d9553d850ab6667f64f229
Patrick Lewis <pllewis1@gmail.com>
2015-10-25 13:05:42 -0500

    wknd calc

--- main.js
+++ main.js
@@ -1,22 +1,58 @@
-var x, y;
-// function getValues()
-// {
-//     x = Number(document.getElementById('num_one').value);
-//     y = Number(document.getElementById('num_two').value);
-//
-//     return x, y;
-// }
+var num1;
+var num2;
+var operator;
+var answer=0;
+
+function dothemath(pass){
+pass.addEventListener("click", function() {
+  num1 = document.getElementById('window').value;
+  document.getElementById('window').value = "";
+  operator = pass.value;
+});
+
+}
+
 
 document.addEventListener("DOMContentLoaded", function(event){
-  document.getElementById("calculateBtn").addEventListener("click", function(event){
-function sum(x, y)
-{
-  x = Number(document.getElementById('num_one').value);
-  y = Number(document.getElementById('num_two').value);
-  return x + y;
-  // document.getElementById("answer").innerHTML = z;
+  function clickHandler(event){
+  event.addEventListener("click", function() {
+    document.getElementById('window').value += event.value;
+  });
 };
 
-document.getElementById('answer').innerHTML = sum(x, y).toString();
-});
+clickHandler(document.getElementById("num7"));
+clickHandler(document.getElementById("num8"));
+clickHandler(document.getElementById("num9"));
+clickHandler(document.getElementById("num4"));
+clickHandler(document.getElementById("num5"));
+clickHandler(document.getElementById("num6"));
+clickHandler(document.getElementById("num3"));
+clickHandler(document.getElementById("num2"));
+clickHandler(document.getElementById("num1"));
+clickHandler(document.getElementById("num0"));
+clickHandler(document.getElementById("dot"));
+dothemath(document.getElementById("divide"));
+dothemath(document.getElementById("minus"));
+dothemath(document.getElementById("times"));
+dothemath(document.getElementById("plus"));
+
+document.getElementById("equals").addEventListener("click", function() {
+  num2 = document.getElementById('window').value;
+  if (operator === "/") {
+    answer = num1/num2;
+  }
+  else if (operator === "x") {
+    answer = num1*num2;
+  }
+  else if (operator === "-") {
+    answer = num1-num2;
+  }
+  else if (operator === "+") {
+    answer = num1.value + num2.value;
+  }
+  document.getElementById('window').value=answer;
+})
+  document.getElementById("clear").addEventListener("click", function(){
+  document.getElementById('window').value = "";
+  });
 });
